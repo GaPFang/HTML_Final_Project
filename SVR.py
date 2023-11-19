1273164ead41c82ad53bc1e3cbb174258296b461
jialin <jlfang0119@gmail.com>
2023-11-19 15:53:22 +0800

    SVR parallel by fork

--- SVR.py
+++ SVR.py
@@ -6,7 +6,7 @@ import os
 import sys
 import random
 import matplotlib.pyplot as plt
-import json
+from time import sleep
 
 total_day = 23 + 16
 total_time = 72
@@ -17,11 +17,48 @@ repeat = 1
 
 y_all, x_all = svm_read_problem("train.txt")
 
-C = sys.argv[2]
-gamma = sys.argv[3]
-epsilon = sys.argv[4]
+C = [0.1, 1, 10, 100, 1000]
+gamma = [0.1, 1, 10, 100, 1000]
+epsilon = [0.1, 1, 10, 100, 1000]
 
-print("C = " + str(C) + ", gamma = " + str(gamma) + ", epsilon = " + str(epsilon), file=sys.stderr)
+# print("C = " + str(C) + ", gamma = " + str(gamma) + ", epsilon = " + str(epsilon))
+
+f = open("result.txt", "w")
+E_CV = np.zeros((5, 5, 5)).tolist()
+read_pipe = np.zeros((5, 5, 5)).tolist()
+for r in range(repeat):
+    mess = np.random.permutation(N).tolist()
+    fold = [mess[:int(N/5)]] + [mess[int(N/5):int(2 * N/5)]] + [mess[int(2 * N/5):int(3 * N/5)]] + [mess[int(3 * N/5):int(4 * N/5)]] + [mess[int(4 * N/5):N]]
+    for i in range(5):
+        for j in range(5):
+            for k in range(5):
+                param = svm_parameter('-s 3 -t 2 -c ' + str(C[i]) + ' -g ' + str(gamma[j]) + ' -p ' + str(epsilon[k]) + ' -q')
+                pid = os.fork()
+                if pid == 0:
+                    for v in range(5):
+                        x_train = []
+                        y_train = []
+                        x_val = []
+                        y_val = []
+                        for j in range(5):
+                            if j == v:
+                                for k in range(int(N/5)):
+                                    x_val.append(x_all[fold[j][k]])
+                                    y_val.append(y_all[fold[j][k]])
+                            else:
+                                for k in range(int(N/5)):
+                                    x_train.append(x_all[fold[j][k]])
+                                    y_train.append(y_all[fold[j][k]])
+                        prob = svm_problem(y_train, x_train)
+                        m = svm_train(prob, param)
+                        p_label, p_acc, p_val = svm_predict(y_val, x_val, m)
+                        # E_CV[i][j][k] += v
+                        E_CV[i][j][k] += (100 - p_acc[0]) / 100
+                    E_CV[i][j][k] /= 5
+                    f.write("C = " + str(C[i]) + ", gamma = " + str(gamma[j]) + ", epsilon = " + str(epsilon[k]) + ", E_CV = " + str(E_CV[i][j][k]) + "\n")
+                    sys.exit(0)
+f.close()
+
 
 # E_CV = np.zeros((5, 5, 5)).tolist()
 # for i in range(5):
@@ -33,7 +70,20 @@ print("C = " + str(C) + ", gamma = " + str(gamma) + ", epsilon = " + str(epsilon
 #                 fold = [mess[:int(N/5)]] + [mess[int(N/5):int(2 * N/5)]] + [mess[int(2 * N/5):int(3 * N/5)]] + [mess[int(3 * N/5):int(4 * N/5)]] + [mess[int(4 * N/5):N]]
 #                 for v in range(5):
 #                     x_train = []
+#                     y_train = []n = []
 #                     y_train = []
+#                     x_val = []
+#                     y_val = []
+#                     for j in range(5):
+#                         if j == v:
+#                             for k in range(int(N/5)):
+#                                 x_val.append(x_all[fold[j][k]])
+#                                 y_val.append(y_all[fold[j][k]])
+#                         else:
+#                             for k in range(int(N/5)):
+#                                 x_train.append(x_all[fold[j][k]])
+#                                 y_train.append(y_all[fold[j][k]])
+#                     prob = svm_problem(y_train, x_train)
 #                     x_val = []
 #                     y_val = []
 #                     for j in range(5):
@@ -50,6 +100,4 @@ print("C = " + str(C) + ", gamma = " + str(gamma) + ", epsilon = " + str(epsilon
 #                     p_label, p_acc, p_val = svm_predict(y_val, x_val, m)
 #                     E_CV[i][j][k] += ((1 - p_acc[0]) / 100)
 #             E_CV[i][j][k] /= (5 * repeat)
-#             print("C = " + str(C[i]) + ", gamma = " + str(gamma[j]) + ", epsilon = " + str(epsilon[k]) + ", E_CV = " + str(E_CV[i][j][k]), file=sys.stderr)
-
-        
+#             print("C = " + str(C[i]) + ", gamma = " + str(gamma[j]) + ", epsilon = " + str(epsilon[k]) + ", E_CV = " + str(E_CV[i][j][k]) + "\n")
