be41776885843a8009a433971a511e9c4dd73fd3
jialin <jlfang0119@gmail.com>
2023-11-23 23:26:33 +0800

    SVR

--- SVR.py
+++ SVR.py
@@ -7,84 +7,96 @@ import sys
 import random
 import matplotlib.pyplot as plt
 from collections import OrderedDict
+import json
+import time
+import math
 
-def calError(y, y_hat):
-    return np.sum(np.abs(y - y_hat)) / len(y)
+C = [0.1, 1, 10, 100, 1000]
+gamma = [0.01, 0.1, 1, 10, 100]
+epsilon =  [0.01, 0.1, 1, 10, 100]
 
-fold = 1000
-total_day = 23 + 16
-total_time = 72
-total_station = 1322
-N = 3704832
+f = open("/dev/null", "w")
+os.dup2(f.fileno(), sys.stdout.fileno())
+f.close()
 
+def calError(y_hat, y, x):
+    err = 0
+    for i in range(len(y)):
+        err += 3 * abs((y_hat[i] - y[i]) / x[i][3]) * (abs(y_hat[i] / x[i][3] - 1 / 3) + abs(y_hat[i] / x[i][3] - 2 / 3))
+    return err / len(y)
+
+fold = 10
 repeat = 10
 
-y_all, x_all = svm_read_problem("train.txt")
-x_all = x_all[:N]
-y_all = y_all[:N]
-
-C = [10, 100, 1000]
-gamma = [1, 10, 100]
-epsilon = [0.01, 0.1, 1]
-
-# print("C = " + str(C) + ", gamma = " + str(gamma) + ", epsilon = " + str(epsilon))
-
-proFd = open("progress.txt", "w")
-read_pipe = np.zeros((len(C), len(gamma), len(epsilon))).tolist()
-mess = np.random.permutation(N).tolist()
-folds = []
-read_pipe = np.zeros((len(C), len(gamma), len(epsilon))).tolist()
-for v in range(fold):
-    folds.append(mess[int(v * N / fold):int((v + 1) * N / fold)])
-for i in range(len(C)):
-    for j in range(len(gamma)):
-        for k in range(len(epsilon)):
-            param = svm_parameter('-s 3 -t 2 -c ' + str(C[i]) + ' -g ' + str(gamma[j]) + ' -p ' + str(epsilon[k]) + ' -q')
-            r, w = os.pipe()
-            pid = os.fork()
-            if pid == 0:
-                E_CV = 0
-                os.close(r)
-                for r in range(repeat):
-                    np.random.seed(np.random.seed(r))
-                    for v in range(fold):
-                        x_train = []
-                        y_train = []
-                        x_val = []
-                        y_val = []
-                        for a in range(fold):
-                            if a == v:
-                                for b in range(int(N/fold)):
-                                    x_val.append(x_all[folds[a][b]])
-                                    y_val.append(y_all[folds[a][b]])
-                            else:
-                                for b in range(int(N/fold)):
-                                    x_train.append(x_all[folds[a][b]])
-                                    y_train.append(y_all[folds[a][b]])
-                        prob = svm_problem(y_train, x_train)
-                        m = svm_train(prob, param)
-                        # p_label, p_acc, p_val = svm_predict(y_val, x_val, m)
-                        # E_CV += v
-                        # E_CV += p_acc[1]
-                        proFd.write("i, j, k, v = " + str(i) + ", " + str(j) + ", " + str(k) + ", " + str(v) + "\n")
-                        proFd.flush()
-                proFd.close()
-                E_CV /= (repeat * fold)
-                os.write(w, str(E_CV).encode('utf-8'))
-                os._exit(0)
-            os.close(w)
-            read_pipe[i][j][k] = r
-
-result = {}
-for i in range(len(C)):
-    for j in range(len(gamma)):
-        for k in range(len(epsilon)):
-            E_CV = float(os.read(read_pipe[i][j][k], 100).decode('utf-8'))
-            result.update({E_CV: [C[i], gamma[j], epsilon[k]]})
-result = OrderedDict(sorted(result.items()))
-max_key_length = max(len(str(key)) for key in result)
-
-f = open("result.txt", "w")
-for i in result:
-    f.write("E_CV: " + str(i).ljust(max_key_length) + " C: " + str(result[i][0]) + " gamma: " + str(result[i][1]) + " epsilon: " + str(result[i][2]) + "\n")
+stationList = []
+f = open("./stationList.txt", "r")
+for line in f:
+    stationList.append(line[:-1])
 f.close()
+
+x_all = []
+y_all = []
+for i in range(len(stationList)):
+    x_all.append([])
+    y_all.append([])
+    path = "./train/train_" + stationList[i] + ".txt"
+    y_all[i], x_all[i] = svm_read_problem(path)
+
+# i = int(input("index: ")) - 1
+
+if os.path.exists("models") == False:
+    os.mkdir("models")
+# proFd = open("progress.txt", "w")
+# resultFd = open("result" + str(i + 1) + ".txt", "w")
+# resultFd = open("result.txt", "w")
+# read_pipe = np.zeros((len(C), len(gamma), len(epsilon))).tolist()
+count = 0
+index = int(sys.argv[1]) - 1
+for l in range(math.ceil(index * len(stationList) / 5), math.ceil((index + 1) * len(stationList) / 5)):
+    pid = os.fork()
+    N = len(x_all[l])
+    count += 1
+    if pid == 0:
+        result = {}
+        E_CV = np.zeros((len(C), len(gamma), len(epsilon))).tolist()
+        for i in range(len(C)):
+            for j in range(len(gamma)):
+                for k in range(len(epsilon)):
+                    param = svm_parameter('-s 3 -t 2 -c ' + str(C[i]) + ' -g ' + str(gamma[j]) + ' -p ' + str(epsilon[k]) + ' -q')
+                    for r in range(repeat):
+                        np.random.seed(np.random.seed(r))
+                        mess = np.random.permutation(N).tolist()
+                        folds = []
+                        for v in range(fold):
+                            folds.append(mess[int(v * N / fold):int((v + 1) * N / fold)])
+                        for v in range(fold):
+                            x_train = []
+                            y_train = []
+                            x_val = []
+                            y_val = []
+                            for a in range(fold):
+                                if a == v:
+                                    for b in range(int(N/fold)):
+                                        x_val.append(x_all[l][folds[a][b]])
+                                        y_val.append(y_all[l][folds[a][b]])
+                                else:
+                                    for b in range(int(N/fold)):
+                                        x_train.append(x_all[l][folds[a][b]])
+                                        y_train.append(y_all[l][folds[a][b]])
+                            prob = svm_problem(y_train, x_train)
+                            m = svm_train(prob, param)
+                            p_label, p_acc, p_val = svm_predict(y_val, x_val, m)        
+                            E_CV[i][j][k] += calError(p_label, y_val, x_val)
+                    E_CV[i][j][k] /= (fold * repeat)
+                    result.update({E_CV[i][j][k]: [C[i], gamma[j], epsilon[k]]})
+        result = OrderedDict(sorted(result.items()))
+        max_key_length = max(len(str(key)) for key in result)
+        f = open("models/model_500101001.txt", "w")
+        for key in result:
+            f.write(str(result[key][0]) + " " + str(result[key][1]) + " " + str(result[key][2]) + " E_CV: " + str(r).ljust(max_key_length) + "\n")
+        f.close()
+        os._exit(0)
+    if count >= 30:
+        os.wait()
+        count -= 1
+        
