5b4a668cfb3ac71282ede4658af43bddbcda468e
jialin <jlfang0119@gmail.com>
2023-11-23 22:22:07 +0800

    preprocess

--- preprocess.py
+++ preprocess.py
@@ -15,22 +15,24 @@ def calWeek(date):
     return (day + 6) % 7
 
 stationList = []
-f = open("./html.2023.final.data/sno_test_set.txt", "r")
-for line in f:
-    stationList.append(line.strip())
+f = open("./html.2023.final.data/demographic.json", "r")
+data = json.load(f)
+for station in data:
+    stationList.append(station)
 f.close()
 
-i = 0
 train_x = []
 train_y = []
+dateList = os.listdir("./html.2023.final.data/release/")
 for i in range(len(stationList)):
     train_x.append([])
     train_y.append([])
-dateList = os.listdir("./html.2023.final.data/release/")
-for date in dateList:
-    week = calWeek(date)
-    for i in range(len(stationList)):
-        f = open("./html.2023.final.data/release/" + date + "/" + stationList[i] + ".json", "r")
+    for date in dateList:
+        week = calWeek(date)
+        path = "./html.2023.final.data/release/" + date + "/" + stationList[i] + ".json"
+        if os.path.exists(path) == False:
+            continue
+        f = open(path, "r")
         data = json.load(f)
         f.close()
         flag = False
@@ -40,16 +42,19 @@ for date in dateList:
             if flag:
                 continue
             if data[time] != {}:
+                flag = True
+                if int(data[time]['act']) == 0:
+                    continue
                 x = {}
-                x.update({1: int(date)})
+                x.update({1: week})
                 x.update({2: int(time[:2]) * 60 + int(time[3:])})
                 x.update({3: int(data[time]['tot'])})
-                x.update({4: int(data[time]['act'])})
-                x.update({5: week})
+                # x.update({4: int(data[time]['act'])})
                 train_x[i].append(x)
                 train_y[i].append(int(data[time]['sbi']))
-                flag = True
 
+if os.path.exists("train") == False:
+    os.mkdir("train")
 f = []
 for i in range(len(stationList)):
     f.append(open("train/train_" + stationList[i] + ".txt", "w"))
@@ -59,5 +64,14 @@ for i in range(len(train_x)):
         for k in train_x[i][j]:
             f[i].write(str(k) + ":" + str(train_x[i][j][k]) + " ")
         f[i].write("\n")
-for i in range(len(stationList)):
+
+for i in range(len(stationList) - 1, 1, -1):
     f[i].close()
+    if os.path.getsize("train/train_" + stationList[i] + ".txt") == 0:
+        os.remove("train/train_" + stationList[i] + ".txt")
+        stationList.pop(i)
+
+f = open("./stationList.txt", "w")
+for station in stationList:
+    f.write(station + "\n")
+f.close()
